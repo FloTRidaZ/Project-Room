02bf8ad9bca79c51f06587198d4c55c7be1860e9
Lisova_Anastasia <anonim15975@gmail.com>
2020-05-22 17:03:22 +0300

    CameraController
    Relized camera controller

--- ProjectRoom/Assets/PlayerController.cs
+++ ProjectRoom/Assets/PlayerController.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/**
+ * Класс для управления игроком 
+ * и камерой по осям X и Y
+ * 
+ * @author Лисова Анастасия, 17ИТ17
+ */
 public class PlayerController : MonoBehaviour {
+    public GameObject cam;
+    Quaternion StartingRotation;
+    float Ver, Hor, RotHor, RotVer;
+    readonly float Speed = 5;
 
-    float Ver, Hor;
-    float Speed = 5;
-	
-	void Update () {
+    private void Start() {
+        StartingRotation = transform.rotation;
+    }
+
+    void Update () {
+        RotHor += Input.GetAxis("Mouse X") * Speed;
+        RotVer += Input.GetAxis("Mouse Y") * Speed;
+
+        RotVer = Mathf.Clamp(RotVer, -60, 60);
+
+        Quaternion RotY = Quaternion.AngleAxis(RotHor, Vector3.up);
+        Quaternion RotX = Quaternion.AngleAxis(-RotVer, Vector3.right);
+
+        transform.rotation = StartingRotation * RotY;
+        cam.transform.rotation = StartingRotation * transform.rotation * RotX;
+
         Ver = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
         Hor = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
 
