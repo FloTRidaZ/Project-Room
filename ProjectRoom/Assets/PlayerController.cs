4b250d758e21b84a564e0a767c972f46fe9c1874
Lisova_Anastasia <anonim15975@gmail.com>
2020-05-22 16:34:00 +0300

    PlayerController
    Realized player controller

---
+++ ProjectRoom/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float Ver, Hor;
+    float Speed = 5;
+	
+	void Update () {
+        Ver = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+        Hor = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+
+        transform.Translate(new Vector3(Hor, 0, Ver));
+	}
+}
