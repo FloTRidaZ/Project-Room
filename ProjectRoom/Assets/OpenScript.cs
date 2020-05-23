3a28b1bd010aa3c7fa1901b66a07d880f8744509
Karasev_Sergey <karasevsergey479@gmail.com>
2020-05-23 14:47:38 +0300

    Добавление тумбочки
    Был добавлен ассет тумбочка, реализовано открытие 1 ящика тумбочки по нажатии на клавишу E

---
+++ ProjectRoom/Assets/OpenScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenScript : MonoBehaviour {
+	Vector3 originPosition;
+	float targetZ;
+	bool opening;
+	// Use this for initialization
+	void Start () {
+		originPosition = transform.position;
+		opening = false;
+		targetZ = originPosition.z - 0.3f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.E) || opening) {
+			float z = transform.position.z - 0.01f;
+			if (z <= targetZ) {
+				opening = false;
+			} else {
+				opening = true;
+			}
+			z = Mathf.Clamp (z, targetZ, originPosition.z);
+			transform.position = new Vector3 (originPosition.x, originPosition.y, z);
+		}
+	}
+}
