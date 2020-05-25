ea20b7e7b77c339853ab1056634e93d13c182d37
Sotnikov_Roman <Sotnikov.Roman58@gmail.com>
2020-05-25 20:28:19 +0300

    DemoScene
    DemoScene

---
+++ ProjectRoom/Assets/DemoClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoClick : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject item = hit.collider.gameObject;
+                if (item != null)
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
