ade1e664716e949bcb203a88b0d5a34a6f366480
Lisova_Anastasia <anonim15975@gmail.com>
2020-05-26 09:59:24 +0300

    CameraShake
    Relized camera shake and run on button LeftShift

---
+++ ProjectRoom/Assets/CameraShake.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/**
+ * Класс, реализующий потрясывание 
+ * камеры при движении игрока
+ * 
+ * @author Лисова Анастасия, 17ИТ17
+ */
+public class CameraShake : MonoBehaviour {
+    public float amount = 2;
+    public float speed = 2;
+
+    private Vector3 startPos;
+    private float distation;
+    private Vector3 rotation = Vector3.zero;
+
+	void Start () {
+        startPos = transform.position;
+	}
+
+	void Update () {
+        distation += (transform.position - startPos).magnitude;
+        startPos = transform.position;
+        rotation.z = Mathf.Sin(distation * speed) * amount;
+        transform.localEulerAngles = rotation;
+	}
+}
