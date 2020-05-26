002e3a2f4042e18007c712db341f4430d506d821
Sotnikov_Roman <Sotnikov.Roman58@gmail.com>
2020-05-26 16:09:58 +0300

    Выделение ячейки инвентаря
    Выделение ячейки инвентаря при наведении на нее курсором мыши

---
+++ ProjectRoom/Assets/Scripts/SelectingSlot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectingSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite activeCell;
+    Sprite cell;
+    Image img;
+
+    // Use this for initialization
+    void Start () {
+        img = GetComponent<Image>();
+        cell = img.sprite;
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        img.sprite = activeCell;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        img.sprite = cell;
+    }
+
+    private void OnDisable()
+    {
+        img.sprite = cell;
+    }
+}
