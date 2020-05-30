4ec9326c4288f479a4d27f04910066645f3ea887
Kruchinina_Kristina <kris.krchnn@gmail.com>
2020-05-30 13:03:13 +0300

    Вращение объекта Android
    Реализация вращения объекта для андроид устройств

---
+++ ProjectRoomAndroid/Assets/RotationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotationScript : MonoBehaviour, IDragHandler  {
+	public GameObject current;
+	Quaternion origin;
+	Quaternion targetX;
+	Quaternion targetY;
+	float deltaY;
+	float deltaX;
+
+	void Start (){
+		origin = current.transform.rotation;
+	}
+	
+	public void OnDrag (PointerEventData eventData){
+		deltaY += eventData.delta.y;
+		deltaX += eventData.delta.x;
+		targetY = Quaternion.AngleAxis (deltaX, Vector3.up);
+		targetX = Quaternion.AngleAxis (deltaY, Vector3.right);
+		current.transform.rotation = origin * targetX * targetY;
+	}
+}
