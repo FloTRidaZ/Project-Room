14b2b7b4d99f61ea74fd098f5b1ae2e0280acd13
Lisova_Anastasia <anonim15975@gmail.com>
2020-05-30 11:49:37 +0300

    Chande CameraController
    Changed camera controller for android platform

--- ProjectRoomAndroid/Assets/Scripts/Player.cs
+++ ProjectRoomAndroid/Assets/Scripts/Player.cs
@@ -2,19 +2,37 @@
 
 /**
  * Класс для управления 
- * игроком по осям X и Y
+ * игроком и камерой 
+ * по осям X и Y 
  * 
  * @author Лисова Анастасия, 17ИТ17
  */
 public class Player : MonoBehaviour {
+    public TouchCamera touchField;
     public Joystick joystick;
-    float Hor, Ver;
+    float Hor, Ver, yRot, xRot;
     readonly float Speed = 2;
+    public GameObject cam;
+    Quaternion StartingRotation;
+
+    void Start()
+    {
+        StartingRotation = transform.rotation;
+    }
 
     void FixedUpdate () {
         Hor = joystick.Horizontal() * Time.deltaTime * Speed;
         Ver = joystick.Vertical() * Time.deltaTime * Speed;
-
         transform.Translate(new Vector3(Hor, 0, Ver));
+
+        xRot += touchField.TouchDist.x / 20;
+        yRot += touchField.TouchDist.y / 20;
+        yRot = Mathf.Clamp(yRot, -60, 60);
+        
+        Quaternion RotY = Quaternion.AngleAxis(xRot, Vector3.up);
+        Quaternion RotX = Quaternion.AngleAxis(-yRot, Vector3.right);
+
+        transform.rotation = StartingRotation * RotY;
+        cam.transform.rotation = StartingRotation * transform.rotation * RotX;
     }
 }
---
+++ ProjectRoomAndroid/Assets/Scripts/TouchCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+/**
+ * Класс для управления камерой,
+ * поворачивающейся в зависимости 
+ * от того, куда свайпает игрок
+ * 
+ * @author Лисова Анастасия, 17ИТ17
+ */
+public class TouchCamera : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+    public Vector2 TouchDist;
+    Vector2 PointerOld;
+    int PointerId;
+    public bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Pressed = true;
+        PointerId = eventData.pointerId;
+        PointerOld = eventData.position;
+    }
+
+    private void Update() {
+        if (Pressed)  {
+            if(PointerId >= 0 && PointerId < Input.touches.Length) {
+                TouchDist = Input.touches[PointerId].position - PointerOld;
+                PointerOld = Input.touches[PointerId].position;
+            }
+            else {
+                TouchDist = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - PointerOld;
+                PointerOld = Input.mousePosition;
+            }
+        }
+        else {
+            TouchDist = new Vector2();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        Pressed = false;
+    }
+}
