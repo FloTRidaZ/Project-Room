884b8349e4ed7f7cb98f3185f668c793478f0e58
Kruchinina_Kristina <kris.krchnn@gmail.com>
2020-06-01 13:45:43 +0300

    Документирование кода
    Документирование класса RotationScript

--- ProjectRoomAndroid/Assets/RotationScript.cs
+++ ProjectRoomAndroid/Assets/RotationScript.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+/**
+ * Класс, позволяющий игроку
+ * вращать объект по осям x и y
+ * при помощи свайпов
+ */ 
 public class RotationScript : MonoBehaviour, IDragHandler  {
-	public GameObject current;
+	public GameObject parent;
 	Quaternion origin;
 	Quaternion targetX;
 	Quaternion targetY;
@@ -12,14 +16,14 @@ public class RotationScript : MonoBehaviour, IDragHandler  {
 	float deltaX;
 
 	void Start (){
-		origin = current.transform.rotation;
+		origin = parent.transform.rotation;
 	}
-	
+
 	public void OnDrag (PointerEventData eventData){
 		deltaY += eventData.delta.y;
 		deltaX += eventData.delta.x;
 		targetY = Quaternion.AngleAxis (deltaX, Vector3.up);
 		targetX = Quaternion.AngleAxis (deltaY, Vector3.right);
-		current.transform.rotation = origin * targetX * targetY;
+		parent.transform.rotation = origin * targetY * targetX;
 	}
 }
