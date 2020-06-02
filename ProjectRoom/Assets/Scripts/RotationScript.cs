210e3342474ea10e8174e03b0a2857048d03d00a
Sotnikov_Roman <Sotnikov.Roman58@gmail.com>
2020-06-02 11:07:46 +0300

    Документация (Коммит Кристины)
    Документация класса RotationScript (Коммит Кристины)

--- ProjectRoom/Assets/Scripts/RotationScript.cs
+++ ProjectRoom/Assets/Scripts/RotationScript.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/**
+ * Класс, позволяющий игроку
+ * вращать объект по осям x и y
+ * при помощи мыши
+ * 
+ * @author Kristina Kruchinina
+ */ 
 public class RotationScript : MonoBehaviour {
 	GameObject rotatedObj;
 	Quaternion origin;
@@ -11,14 +17,18 @@ public class RotationScript : MonoBehaviour {
 	float speed = 6.5f;
 	float mouseX;
 	float mouseY;
-
+	/**
+	 * Запускается при инициализации сцены
+	 */ 
 	void Awake () {
 		rotatedObj = Instantiate(Resources.Load<GameObject>(CurrentItem.currentPath));
 		rotatedObj.GetComponent<Rigidbody> ().isKinematic = true;
 		rotatedObj.transform.parent = gameObject.transform;
 		rotatedObj.transform.position = gameObject.transform.position;
 	}
-
+	/**
+	 * Запускается перед 1 фреймом
+	 */ 
 	void Start () {
 		origin = transform.rotation;
 	}
@@ -33,7 +43,7 @@ public class RotationScript : MonoBehaviour {
 		mouseX += Input.GetAxis ("Mouse X") * speed;
 		mouseY += Input.GetAxis ("Mouse Y") * speed;
 		rotationY = Quaternion.AngleAxis (mouseX, Vector3.up);
-		rotationX = Quaternion.AngleAxis (mouseY, Vector3.right);
-		transform.rotation = origin * rotationX * rotationY;
+		rotationX = Quaternion.AngleAxis (-mouseY, Vector3.right);
+		transform.rotation = origin * rotationY * rotationX;
 	}
 }
