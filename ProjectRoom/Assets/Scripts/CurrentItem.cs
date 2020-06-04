965c93b74404104bc35f6116067a1f53c76fb892
Sotnikov_Roman <Sotnikov.Roman58@gmail.com>
2020-06-04 14:05:05 +0300

    HighlightingACell и CurrentItem объединены в один класс
    HighlightingACell и CurrentItem объединены в один класс

--- ProjectRoom/Assets/Scripts/CurrentItem.cs
+++ ProjectRoom/Assets/Scripts/CurrentItem.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using System;
+using UnityEngine.UI;
 
 /**
  * Класс, реализующий работу выбранного объекта в инвентаре
  *
  * @author Сотников Р. 17ит17
  */
-public class CurrentItem : MonoBehaviour, IPointerClickHandler
+public class CurrentItem : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
-	public static string currentPath;
+    public static string currentPath;
+
+    [Header("Подсветка")]
+    public Sprite activeCell;
 
     [HideInInspector]
     public int index;
     GameObject inventoryObject;
     Inventory inventory;
+    Sprite cell;
+    Image img;
 
     // Use this for initialization
     void Start()
     {
         inventoryObject = GameObject.FindGameObjectWithTag("InventoryManager");
         inventory = inventoryObject.GetComponent<Inventory>();
+    }   
+    
+    // Use this for initialization
+    void Awake()
+    {
+        img = GetComponent<Image>();
+        cell = img.sprite;
     }
 
     /**
@@ -45,5 +58,29 @@ public class CurrentItem : MonoBehaviour, IPointerClickHandler
                 SceneManager.LoadScene("Rotation");
             }
         }
+    }
+
+    /**
+     * Если положение курсора совпадает с ячейкой инвентаря, то включается ее подсветка
+     */
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        img.sprite = activeCell;
+    }
+
+    /**
+     * Если положение курсора не совпадает с ячейкой инвентаря, то подсветка выключается
+     */
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        img.sprite = cell;
+    }
+
+    /**
+     * После закрытия инвентаря у всех ячеек выключается подсветка
+     */
+    private void OnDisable()
+    {
+        img.sprite = cell;
     }
 }
